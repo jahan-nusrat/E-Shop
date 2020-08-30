c0a0c8abfd696cd3ca15caad9a261674e7aefe07
Nusrat Jahan <njnusrat53@gmail.com>
2020-08-30 23:19:58 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App () {
+	return (
+		<Router>
+			<div className="app">
+				<Switch>
+					<Route path="/checkout">
+						<h1>Checkout</h1>
+					</Route>
+					<Route path="/login">
+						<h1>Log In Page</h1>
+					</Route>
+					<Route path="/">
+						<h1>Home</h1>
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* const firebaseConfig = {
+    apiKey: "AIzaSyDu81B2Gfia5FMbTweRjjTNJcxkndOCVkc",
+    authDomain: "clone-c2da3.firebaseapp.com",
+    databaseURL: "https://clone-c2da3.firebaseio.com",
+    projectId: "clone-c2da3",
+    storageBucket: "clone-c2da3.appspot.com",
+    messagingSenderId: "11237417630",
+    appId: "1:11237417630:web:b83c96f8c1d7f9a9e9d18f",
+    measurementId: "G-RWG88LBPZ8"
+  }; */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
