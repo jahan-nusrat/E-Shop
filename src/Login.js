d2477401936d962a33a23867dca682291fdbcc5f
Nusrat Jahan <njnusrat53@gmail.com>
2020-09-02 09:53:20 +0600

    login form build

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { LoginLogo, LoginContainer } from './LoginStyle';
+import { LoginLogo, LoginContainer, LogIn, LogHead, LoginSecondHead, LoginInput } from './LoginStyle';
 
 function Login () {
 	return (
-		<div className="login">
+		<LogIn className="login">
 			<Link to="/">
 				<LoginLogo
 					className="login__logo"
@@ -13,20 +13,21 @@ function Login () {
 				/>
 			</Link>
 			<LoginContainer className="logo__container">
-				<h1>Sign-in</h1>
+				<LogHead>Sign-in</LogHead>
 				<form>
-					<h5>E-mail</h5>
-					<input type="text" />
-					<h5>Password</h5>
-					<input type="password" />
+					<LoginSecondHead>E-mail</LoginSecondHead>
+					<LoginInput type="text" />
+					<LoginSecondHead>Password</LoginSecondHead>
+					<LoginInput type="password" />
 					<button>Sign In</button>
 				</form>
 				<p>
 					By signing-in you agree to Amazon's conditions of Use & Sale. Please see our Privacy Policy, our
 					Cookies Notice and our internet-based Ads Notice.
 				</p>
+				<button>Create your Amazon Account</button>
 			</LoginContainer>
-		</div>
+		</LogIn>
 	);
 }
 
--- src/LoginStyle.js
+++ src/LoginStyle.js
@@ -2,11 +2,33 @@ import styled from 'styled-components';
 
 export const LoginLogo = styled.img`
 	width: 100px;
-	object-fit: cover;
-	display: block;
-	margin: 5px auto;
+	object-fit: contain;
+
+	margin: 20px auto;
 `;
 export const LoginContainer = styled.div`
-	width: 30%;
-	margin: auto;
+	width: 350px;
+	display: flex;
+	flex-direction: column;
+	padding: 20px;
+	border: 1px solid lightgray;
+`;
+export const LogIn = styled.div`
+	background-color: #ffffff;
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+export const LogHead = styled.h1`
+	font-weight: 500;
+	margin-bottom: 1.2rem;
+`;
+export const LoginSecondHead = styled.h5`margin-bottom: 5px;`;
+export const LoginInput = styled.input`
+	height: 30px;
+	width: 98%;
+	background-color: #ffffff;
+	margin-bottom: 10px;
+	border: 1.5px solid #000000;
 `;
